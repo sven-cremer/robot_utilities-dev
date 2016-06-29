66cec4c1c10b1d6014978e8a3a7d33b6f02f8c9b
anilmullapudi <anilmullapudi@gmail.com>
2016-06-29 10:06:07 -0500

    msm state machine is modified

--- apc_sm/src/StateMachine_Node.cpp
+++ apc_sm/src/StateMachine_Node.cpp
@@ -28,35 +28,15 @@ int main(int argc, char **argv) {
 		pickupMachine p;
 		// needed to start the highest-level SM. This will call on_entry and mark the start of the SM
 		p.start();
+		pstate(p);
 		p.process_event(activate());
-		p.process_event(navigate());
-		/*// go to Open, call on_exit on Empty, then action, then on_entry on Open
-		p.process_event(open_close());
 		pstate(p);
-		p.process_event(open_close());
+		/*p.process_event(navigate());
 		pstate(p);
-		// will be rejected, wrong disk type
-		p.process_event(object_detected("louie, louie", DISK_DVD));
-		pstate(p);
-		p.process_event(object_detected("louie, louie", DISK_CD));
-		pstate(p);
-		p.process_event(play());
+		p.process_event(pick("water", BOTTLE));
+		p.process_event(pick("drink", COKE));
 
-		// at this point, Play is active
-		p.process_event(sm::pause());
-		pstate(p);
-		// go back to Playing
-		p.process_event(end_pause());
-		pstate(p);
-		p.process_event(sm::pause());
-		pstate(p);
-		p.process_event(stop());
-		pstate(p);*/
-		// event leading to the same state
-		// no action method called as it is not present in the transition table
-		//p.process_event(stop());
-		//pstate(p);
-		std::cout << "stop fsm" << std::endl;
+		std::cout << "stop fsm" << std::endl;*/
 		p.stop();
 
 		chatter_pub.publish(msg);
