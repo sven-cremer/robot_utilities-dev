40c4638d5748ce289f1feed66f4601babf190674
Sven Cremer <sven.cremer@mavs.uta.edu>
2017-05-13 15:32:14 -0500

    Using system command for data recording;  added Boost for filesystem support.

--- data_recorder/include/data_recorder/data_recorder.h
+++ data_recorder/include/data_recorder/data_recorder.h
@@ -2,7 +2,7 @@
  * data_recorder.h
  *
  *  Created on: May 9, 2017
- *      Author: sven
+ *      Author: Sven Cremer
  */
 
 #ifndef DATA_RECORDER_DATA_RECORDER_H_
@@ -10,25 +10,41 @@
 
 
 #include <ros/ros.h>
-#include <ros/package.h>
-#include <rosbag/bag.h>
+//#include <ros/package.h>
+//#include <rosbag/bag.h>
+
+// Boost
+#include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
 
 class DataRecorder
 {
 public:
 
-	DataRecorder(std::string topic_name);
+	DataRecorder(std::vector<std::string> param_topics,
+                 std::vector<std::string> param_fnames,
+				 std::string param_dataDir,
+				 std::string param_path);
 	~DataRecorder();
 
-	void start(std::string file_name);
+	void start(int file_number);
+	void start();
 	void stop();
 
 private:
 
-	rosbag::Bag bag;
+	//rosbag::Bag bag;
 
-	std::string topic;
+	std::vector<std::string> topics;
+	std::vector<std::string> fnames;
+	std::string dataDir;
+	std::string path;
 
+	void start(std::string pathDataDir);
+	void systemStart(std::string topic, std::string fname, std::string pathDataDir);
+	void systemStop (std::string topic);
+
+	void createDataFolder(std::string pathDataDir);
 };
 
 
--- data_recorder/src/data_recorder.cpp
+++ data_recorder/src/data_recorder.cpp
@@ -7,20 +7,93 @@
 
 #include <data_recorder/data_recorder.h>
 
-DataRecorder::DataRecorder(std::string topic_name)
+DataRecorder::DataRecorder(std::vector<std::string> param_topics,
+                           std::vector<std::string> param_fnames,
+                           std::string param_dataDir,
+                           std::string param_path)
 {
-	topic = topic_name;
+	topics      = param_topics;
+	fnames      = param_fnames;
+	dataDir     = param_dataDir;
+	path        = param_path;
+
+	if( topics.size() != fnames.size() )
+	{
+		std::cerr << "Number of topic and file names do not agree!\n";
+	}
+	if( !boost::filesystem::exists(path) )
+	{
+		std::cerr << "Path does not exist!\n";
+	}
 }
 
 
 DataRecorder::~DataRecorder()
 {
-
 }
 
-
-void DataRecorder::start(std::string file_name)
+void DataRecorder::start(int file_number)
 {
-	bag.open(file_name.c_str(), rosbag::bagmode::Write);
+	std::ostringstream ss;
+	ss << std::setw(2) << std::setfill('0') << file_number;
+
+	std::string pathDataDir = path + "/" + dataDir + "_" + ss.str();
+	start(pathDataDir);
+}
+void DataRecorder::start()
+{
+	std::string pathDataDir = path + "/" + dataDir;
+	start(pathDataDir);
+}
+void DataRecorder::start(std::string pathDataDir)
+{
+	createDataFolder(pathDataDir);
+
+	for(int i=0; i<topics.size();i++)
+	{
+		systemStart(topics[i], fnames[i], pathDataDir);
+	}
+	//bag.open(file_name.c_str(), rosbag::bagmode::Write);
 	//bag.write(topic.c_str());
 }
+
+void DataRecorder::stop()
+{
+	for(int i=0; i<topics.size();i++)
+	{
+		systemStop(topics[i]);
+	}
+}
+
+void DataRecorder::systemStart(std::string topic, std::string fileName, std::string pathDataDir)
+{
+	  std::string cmd = "rostopic echo -p " + topic  + " > " + pathDataDir + "/" + fileName + ".csv &";
+	  std::cout<<"$ "<<cmd.c_str()<<"\n";
+	  system( cmd.c_str() );
+}
+
+void DataRecorder::systemStop(std::string topic)
+{
+	  std::string cmd = "pkill -9 -f " + topic;
+	  std::cout<<"$ "<<cmd.c_str()<<"\n";
+	  system( cmd.c_str() );
+}
+
+void DataRecorder::createDataFolder(std::string pathDataDir)
+{
+	// Create experiment data directory
+	boost::filesystem::path dir(pathDataDir);
+	if(!boost::filesystem::create_directory(dir))
+	{
+		if( boost::filesystem::exists(dir) )
+		{
+			std::cout << "Warning: Experiment folder already exists ... will replace files!" << "\n";
+		}
+		else
+		{
+			std::cerr << "Failed to create experiment directory!" << "\n";
+			//return 1;
+		}
+	}
+}
+
